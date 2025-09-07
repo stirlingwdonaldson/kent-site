3aff93697e9f40452cf49ec6de058b2d038fd19d
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-09-07 15:22:53 +1000

    added so if viewport width < triangle in loading screen, add fill above and below top and bottom triangles

--- kent-site/src/App.tsx
+++ kent-site/src/App.tsx
@@ -13,26 +13,9 @@ function App() {
         <SplashScreen onComplete={() => setShowSplash(false)} />
       ) : (
         <>
-          <div>
-            <a href="https://vite.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => {}}>
-              count is 0
-            </button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
+          
+          <h1>Kent Hotel</h1>
+          
         </>
       )}
     </>
